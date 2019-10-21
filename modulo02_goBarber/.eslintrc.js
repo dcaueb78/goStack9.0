f8909b3e53e51fc7ccff8000815a072f261ff89c
dcaueb78 <caue1017@hotmail.com>
2019-10-21 17:52:50 -0300

    Agendamento finalizado

--- modulo02_goBarber/.eslintrc.js
+++ modulo02_goBarber/.eslintrc.js
@@ -1,29 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  plugins: [
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-params-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePatter": "next" }],
-    "no-param-reassign": "off"
-  },
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePatter: 'next' }]
+  }
 };
--- modulo02_goBarber/src/routes.js
+++ modulo02_goBarber/src/routes.js
@@ -1,17 +1,34 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
+
 // Só vai passar no Middleware o que vier depois do use
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
