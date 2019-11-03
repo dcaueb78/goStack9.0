fe8135c7a222c73ece0be82ee80a7d9f652fa947
dcaueb78 <caue1017@hotmail.com>
2019-11-03 15:02:33 -0200

    Update img size

--- modulo04_facebook/src/components/Post/Post.js
+++ modulo04_facebook/src/components/Post/Post.js
@@ -7,15 +7,17 @@ class Post extends Component {
     return (
       <>
         <div className="post">
-          <img src={this.props.data.author.avatar} />
+          <div>
+            <img src={this.props.data.author.avatar} />
+          </div>
           <div className="author">
             <h1 className="autor-name">{this.props.data.author.name}</h1>
             <h1 className="post-date">{this.props.data.date}</h1>
           </div>
-          <hr/>
+          {/* <hr/>
           <div>
             Coments
-          </div>
+          </div> */}
         </div>
       </>
     );
