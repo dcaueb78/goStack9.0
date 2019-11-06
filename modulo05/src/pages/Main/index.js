497bb2fb3652dbb6179e32487ef4c87ab2cdf1a9
dcaueb78 <caue1017@hotmail.com>
2019-11-06 19:39:13 -0200

    Add local storage

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -9,6 +9,24 @@ import api from '../../services/api';
 class Main extends Component {
   state = { newRepo: '', repositories: [], loading: false };
 
+  // carregar os dados do local storages
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // salvar dados local storage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
