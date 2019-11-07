d9f50a0bec5d2b466579e686eef6aec437cfaf17
dcaueb78 <caue1017@hotmail.com>
2019-11-06 22:46:33 -0200

    Created issues request

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
-export default function Repository({ match }) {
-  return <h1> Repositório: {decodeURIComponent(match.params.repository)}</h1>;
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state();
+
+    return <h1> Repositório: </h1>;
+  }
 }
