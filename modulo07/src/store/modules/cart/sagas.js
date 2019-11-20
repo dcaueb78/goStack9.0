a2c31bf83db48a421e8df708f5833f1a235d878f
dcaueb78 <caue1017@hotmail.com>
2019-11-20 14:36:49 -0200

    Config history

--- modulo07/src/store/modules/cart/sagas.js
+++ modulo07/src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
 
 import { addToCartSuccess, updateAmountSucess } from './actions';
@@ -35,6 +36,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
