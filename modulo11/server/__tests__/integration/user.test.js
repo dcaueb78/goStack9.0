5d6bca9ade6b14432273ec202bddcabf135fa76c
Cauê Kotarski <caue1017@hotmail.com>
2020-04-05 21:24:01 -0300

    Create user password tests

--- modulo11/server/__tests__/integration/user.test.js
+++ modulo11/server/__tests__/integration/user.test.js
@@ -1,16 +1,56 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Cauê Kotarski',
+      email: 'caue1017@hotmail.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Cauê Kotarski',
         email: 'caue1017@hotmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Cauê Kotarski',
+        email: 'caue1017@hotmail.com',
+        password: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Cauê Kotarski',
+        email: 'caue1017@hotmail.com',
+        password: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
